ab2de8564285c904545185dca10149b4b025af38
vineetvermaa61 <vineetvermaa61@gmail.com>
2023-10-19 02:00:05 +0530

    Update lec1_1_TO_n_even.cpp

--- #Main_Codes/Week1_Basics_Of_Programming/lec1_1_TO_n_even.cpp
+++ #Main_Codes/Week1_Basics_Of_Programming/lec1_1_TO_n_even.cpp
@@ -1,20 +1,26 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    int n; cin>>n;
-    for (int i = 2; i <= n; i = i+2)
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 2; i <= n; i = i + 2)
     {
-            cout<<i<<" ";
+        cout << i << " ";
     }
-    
-    cout<<endl<<"Alternate Method"<<endl;
+
+    cout << endl
+         << "Alternate Method" << endl;
     for (int j = 1; j <= n; j++)
     {
-        if(j % 2 == 0){
-            cout<<j<<" ";
+        if (j % 2 == 0)
+        {
+            cout << j << " ";
         }
     }
-    
+
     return 0;
 }
+
+// adding new change 2
